fcefb07ee12e46e3cffd2b730a4ca5f93b9f16c9
Deepak <dev.malik@outlook.com>
2023-09-20 17:57:39 +0200

    First Draft

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrackTableComponent } from './componant/tracks/track-table/track-table.component';
+import { TrackEditComponent } from './componant/tracks/track-edit/track-edit.component';
+import { TrackAddComponent } from './componant/tracks/track-add/track-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path:'tracks', component:TrackTableComponent},
+	{path:'tracks/edit/:id', component:TrackEditComponent},
+	{path:'tracks/add', component:TrackAddComponent}
+
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/data/api.response.ts
+++
@@ -1,7 +0,0 @@
-export interface ApiResponse 
-{   
-    status:bigint;
-    data:any;
-    error:string;
-    message:string;
-}
